78e55c5b50e04ae34eb8741ef96678f2cc5d4a73
baabar02 <baabarmx@gmail.com>
2025-04-06 20:37:22 +0800

    mmh

--- react-pages/src/pages/index.js
+++ react-pages/src/pages/index.js
@@ -1,42 +1,63 @@
-import Card from "../components/Card";
- 
-// const cards = [1,2,3]
-// // return <div>{cards}</div>
- 
-const values = [1, "text", 3, 4, 5, 5];
- 
-const HomePage = () => {
-  return (
-    <div>
-    <Box text="baabar" />
-     <Box text="2" />
-     <Box text="3" />
-     <Box text="4" />
-     <Box text="4" />
-     <Box text="4" />
-      <Card/>
-      <Card/>
+// import Card from "../components/Card";
+  
 
-   
- 
-     
- 
-     
-      {values.map((value, index) => {
-        return <Box key={index} text={value} />;
-      })}
- 
- 
+//  const numbers =[1, 2, 3, 4]
+
+
+
+ const HomePage =() =>{
+
+  return <div>
+  
+    <Box text='1'/>
+    <Box text='1'/>
+    <Box text='1'/>
+    <Box text='1'/>
+    <Box text='1'/>
+
     </div>
-  );
-};
+ }
  
-export default HomePage;
+  
+  // return <div style={{display: "flex", fontSize: 100}}>
+  //   {
+  //   numbers.map((number) => <p>{number}</p>)
+  //   }
+
  
-const Box = (props) => {
-  return <div>{props.text}</div>;
-};
+
+ export default (HomePage);
+
+ const Box = (props) => {
+  console.log('props:', props);
+  
+  return <div style={{marginLeft: 30, marginTop: 30}}>{props.text}</div>
+ }
+
+//  const values = ['text', 'text2','text3']
+ // values.map((value,index)=> {
+    //   return <Box key ={index} text={value}/>
+      
+    // })
+ //reusable
+
+//  const seconds = 1220;
+//  let minutes = Math.floor(seconds/60);
+//  let remainingSeconds = seconds % 60;
+  
+//  console.log(`${minutes} min and ${remainingSeconds} seconds`);
  
-//-----------------
- 
- 
+
+// const numbers = [2,3,5,8,]
+
+// let smallest = numbers.reduce((min, curr) => Math.min(min,curr) ) 
+
+// console.log(smallest);
+
+// let smallest = numbers[0];
+// for(let i = 0; i < numbers.length; i++) {
+//    if(numbers[i] < smallest){
+//       smallest = numbers[i]
+//    }
+// }
+
